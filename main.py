76ab833ea2dfe1bb24345971549f5ac24fc9c4e2
Eom, Daham <deom7@gatech.edu>
2018-11-27 16:50:52 -0500

    sql connection added

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+from flaskext.mysql import MySQL
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
 
 app = Flask(__name__)
 
-@app.route('/')
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
+app.config['MYSQL_DATABASE_DB'] = 'atlzoo'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM admins")
+data = cursor.fetchone()
+print data
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'This is a gom page'
+    print data
+    return 'aha'
 
 if __name__ == '__main__':
     app.run()
