eba804439454b2b1bff9b4396d14b82f1355f125
Eom, Daham <deom7@gatech.edu>
2018-11-30 20:56:50 -0500

    Login portion finished

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash, Blueprint
+from flask import Flask, request, render_template, redirect, url_for, flash
 from flaskext.mysql import MySQL
 from wtforms import Form, StringField, PasswordField, validators
 
@@ -24,7 +24,50 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('Login.html')
+    if request.method == 'POST' and 'cancelReg' in request.form:
+        return render_template('Login.html')
+    elif request.method == 'POST' and 'login' in request.form:
+        validated = loginHelper(request.form)
+        if validated == 0:
+            return render_template('visitorHomes.html')
+        elif validated == 1:
+            return render_template('staffhome.html')
+        elif validated == 2:
+            return render_template('adminhome.html')
+        else:
+            return render_template('badLogin.html')
+    else:
+        return render_template('Login.html')
+
+def loginHelper(list):
+    input_email = str(list['email'])
+    input_password = str(list['password'])
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    db_names = ['visitors', 'staff', 'admins']
+    for db in db_names:
+        cursor.execute("SELECT * FROM " + db + " WHERE Email = %s", (input_email))
+        data = cursor.fetchall()
+        if len(data) == 1:
+            username = data[0][0]
+            password = data[0][1]
+            if db == 'visitor':
+                exception_password = ['password4', 'password5', 'password6', 'password7']
+            elif db == 'staff':
+                exception_password = ['password1', 'password2', 'password3']
+            else:
+                exception_password = ['adminpassword']
+            if check_password_hash(password, input_password) or (password in exception_password and password == input_password):
+                if db == 'visitors': # TODO: pass username
+                    return 0
+                elif db == 'staff':
+                    return 1
+                else:
+                    return 2
+            else:
+                return -1
+    return -1
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -56,13 +99,30 @@ def RegisterUser(list, db_name):
 
         move_to_login = False
         if check_password_hash(password, password2):
+            if db_name == 'visitors':
+                othername = 'staff'
+            else:
+                othername = 'visitors'
+            cursor.execute("SELECT * FROM admins WHERE Username = %s", (username))
+            notHaveName = cursor.fetchone() == None
+            cursor.execute("SELECT * FROM " + othername + " WHERE Username = %s", (username))
+            notHaveName = cursor.fetchone() == None and notHaveName
             cursor.execute("SELECT * FROM " + db_name + " WHERE Username = %s", (username))
-            if cursor.fetchone() == None:
+            notHaveName = cursor.fetchone() == None and notHaveName
+
+            cursor.execute("SELECT * FROM admins WHERE Email = %s", (email))
+            notHaveEmail = cursor.fetchone() == None
+            cursor.execute("SELECT * FROM " + othername + " WHERE Email = %s", (email))
+            notHaveEmail = cursor.fetchone() == None and notHaveName
+            cursor.execute("SELECT * FROM " + db_name + " WHERE Email = %s", (email))
+            notHaveEmail = cursor.fetchone() == None and notHaveName
+
+            if notHaveName and notHaveEmail:
                 cursor.execute("INSERT INTO " + db_name + " (Username, Password, Email) VALUES(%s, %s, %s)", (username, password, email))
                 conn.commit()
                 move_to_login = True
             else:
-                flash('Username already exists')
+                flash('Username or email already exists')
         else:
             flash('Passwords do not match')
         cursor.close()
